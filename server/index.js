59d69cab522e7e219cf090d94c25dbc830faead4
chetan gupta <chetangupta123raj@gmail.com>
2023-06-13 13:00:35 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const config = require('config');
 const log = require('./utils/logger');
 const routes = require('./routes');
+const cors = require('cors');
 const { connect } = require('./utils/connect');
 
 const port = config.get("port");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
