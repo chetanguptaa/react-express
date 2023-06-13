546a980cedd0aeccde519f613943a79be2d41ec5
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-13 18:16:58 +0530

    updated authentication.js

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -12,29 +12,25 @@ const generateRefreshToken = (payload) => {
     return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 };
 
+const auth = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    if(!authHeader) {
+        return res.status(403).json({
+            message: 'authentication token is missing'
+        })
+    }
+    const decoded = jwt.verify(authHeader, ACCESS_TOKEN_SECRET);
+    if(decoded && decoded.id) {
+        req.userId = decoded.id;
+        next();
+    } else {
+        return res.status(403).json({msg: "Incorrect token"});
+    }
+}
+
 module.exports = {
+    auth,
     generateAccessToken,
     generateRefreshToken
-};
+}
 
-// const JWT_SECRET = config.get("JWT_SECRET");
-
-// const auth = (req, res, next) => {
-//     const authHeader = req.headers["authorization"];
-//     if(!authHeader) {
-//         return res.status(403).json({
-//             message: 'authentication token is missing'
-//         })
-//     }
-//     const decoded = jwt.verify(authHeader, JWT_SECRET);
-//     if(decoded && decoded.id) {
-//         req.userId = decoded.id;
-//         next();
-//     } else {
-//         return res.status(403).json({msg: "Incorrect token"});
-//     }
-// }
-
-// module.exports = {
-//     auth
-// }
