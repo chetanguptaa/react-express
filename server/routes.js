2a48fd78f3a4ae6a732662d2f9160f781a00a439
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-13 18:16:46 +0530

    updated routes.js

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {createUserHandler, loginHandler} = require('./controllers/userController');
 const validateResource = require('./middleware/validateResource');
 const createUserSchema = require('./schemas/userSchema');
+const UserModel = require('./models/userModel');
 const routes = express.Router();
+const {auth} = require('./middleware/authentication');
 
 routes.get('/', (req, res) => {
     res.json({
@@ -11,6 +13,10 @@ routes.get('/', (req, res) => {
 })
 routes.post('/signin', validateResource(createUserSchema), createUserHandler);
 routes.post('/login', validateResource(createUserSchema), loginHandler);
+routes.get("/me", auth, (req, res) => {
+    const user = UserModel.find((x) => x.id === req.userId);
+    res.json({ email: user.email, id: user.id });
+});
 
 
 module.exports = routes;
