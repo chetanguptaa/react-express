d2b467a69db846d82c5c3567b6c0c767aab6bd6d
chetan gupta <chetangupta123raj@gmail.com>
2023-06-13 15:42:59 +0530

    updated routes.js

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const {createUserHandler, validatePasswordHandler, loginHandler} = require('./controllers/userController');
 const validateResource = require('./middleware/validateResource');
 const createUserSchema = require('./schemas/userSchema');
-const {  } = require('./middleware/authentication');
-
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
