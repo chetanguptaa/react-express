1b6d2233924c1568167bd81a60925f3bc53ed685
chetan gupta <chetangupta123raj@gmail.com>
2023-06-13 13:00:53 +0530

    updated Signin.jsx

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -9,82 +9,100 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
 
 const defaultTheme = createTheme();
 
 export default function Signin() {
-  const handleSignin = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email')
-    });
-  };
+    const [error, setError] = useState('');
+    console.error(error);
+    const handleSignin = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get('email');
+        const password = data.get('password');
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-        <CssBaseline />
-        <Container component="main" maxWidth="xs" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-            <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                p: 4,
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-                boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
-                backgroundColor: '#fff',
-              }}
-            >
-                <Avatar sx={{ m: 'auto', bgcolor: 'cornflowerblue' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
-                    Sign In
-                </Typography>
-                <Box component="form" noValidate onSubmit={handleSignin} sx={{ mt: 3, width: '100%' }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            />
+        try {
+            const response = await fetch('http://localhost:8000/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email, password}),
+            });
+            const responseData = await response.json();
+            console.log(responseData);
+            event.target.reset();
+            setError('');
+        } catch (error) {
+            setError('An error occured. Please try again.');
+            console.log(error);
+        }
+    };
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            <Container component="main" maxWidth="xs" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    p: 4,
+                    border: '1px solid #ccc',
+                    borderRadius: '4px',
+                    boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
+                    backgroundColor: '#fff',
+                }}
+                >
+                    <Avatar sx={{ m: 'auto', bgcolor: 'cornflowerblue' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
+                        Sign In
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSignin} sx={{ mt: 3, width: '100%' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="new-password"
-                            />
+                        <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        >
+                        Sign In
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link href="/" variant="body2">
+                                    Already have an account? Log in
+                                </Link>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    >
-                    Sign In
-                    </Button>
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            <Link href="/" variant="body2">
-                                Already have an account? Log in
-                            </Link>
-                        </Grid>
-                    </Grid>
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
-    </ThemeProvider>
-  );
+            </Container>
+        </ThemeProvider>
+    );
 }
