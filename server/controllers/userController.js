4f7110a8709f6b542c2d5e4c9eaaa4dc79fca0bd
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-15 20:03:15 +0530

    updated userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,10 @@
 const logger = require('../utils/logger');
 const { createUser } = require('../services/userService');
 const UserModel = require('../models/userModel');
-const bcrypt = require('bcryptjs');
 const { generateAccessToken,generateRefreshToken } = require('../middleware/authentication');
 
 
-const createUserHandler = async (req, res, next) => {
+const createUserHandler = async (req, res) => {
     try {
         const user = await createUser(req.body);
         return res.send(user);
@@ -15,7 +14,7 @@ const createUserHandler = async (req, res, next) => {
     }
 }
 
-const loginHandler = async (req, res, next) => {
+const loginHandler = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await UserModel.findOne({ email });
@@ -29,11 +28,12 @@ const loginHandler = async (req, res, next) => {
         // Here, you can generate a token or session to maintain the user's authenticated state
         const accessToken = generateAccessToken({ userId: user._id });
         const refreshToken = generateRefreshToken({ userId: user._id });
-        return res.json({accessToken, refreshToken});
+        res.json({accessToken, refreshToken, redirectUrl: '/me'});
+        // res.redirect(`/me?access_token=${accessToken}`);
     } catch (e) {
         logger.error(e);
         return res.status(409).send(e.message);
     }
-  };
+};
 
 module.exports = {createUserHandler, loginHandler};
