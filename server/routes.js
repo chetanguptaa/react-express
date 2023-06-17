d5d0f916afc9251b2a61ac8d0e8a786b2836204f
chetan gupta <chetangupta123raj@gmail.com>
2023-06-17 18:40:57 +0530

    updated routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const {createUserHandler, loginHandler} = require('./controllers/userController');
+const {createUserHandler, loginHandler, getUser, getUserFriends, addRemoveFriend} = require('./controllers/userController');
 const validateResource = require('./middleware/validateResource');
 const createUserSchema = require('./schemas/userSchema');
 const UserModel = require('./models/userModel');
 const routes = express.Router();
 const {auth} = require('./middleware/authentication');
 
-routes.get('/', (req, res) => {
+routes.get('/hello', (req, res) => {
     res.json({
         message: 'hello world'
     });
 })
 routes.post('/signin', validateResource(createUserSchema), createUserHandler);
 routes.post('/login', validateResource(createUserSchema), loginHandler);
-routes.get("/me", auth, async (req, res) => {
+routes.get('/me', auth, async (req, res) => {
     try {
         const user = await UserModel.findOne({ id: req.userId });
         if (!user) {
@@ -24,6 +24,12 @@ routes.get("/me", auth, async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 });
+routes.get('/', auth, getFeedPosts);
+routes.get('/:userId/posts', auth, getUserPosts);
+routes.patch('/:id/like', auth, likePost);
+routes.get('/:id', auth, getUser);
+routes.get('/:id/friends', auth, getUserFriends);
+routes.patch('/:id/:friendId', auth, addRemoveFriend);
 
 
 module.exports = routes;
