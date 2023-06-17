31a4d1c4d58fb521210fb9598444321bcc7a6e18
chetan gupta <chetangupta123raj@gmail.com>
2023-06-17 18:41:36 +0530

    updated userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('config');
 
-const UserInput = {
-    email: String,
-    password: String
-};
-
 const userSchema = new mongoose.Schema(
     {
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
+        picturePath: {type: String,default: "",},
+        friends: {type: Array,default: [],},
+        viewedProfile: Number,
+        impressions: Number,
     },
     {
         timestamps: true,
