564e4bd3e93b6f327400a2dcef255a9ce987c83d
chetanguptaa <chetangupta123raj@gmail.com>
2023-06-20 20:10:21 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const config = require('config');
 const log = require('./utils/logger');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/users');
-const postRoutes = require('./routes/posts');
 const cors = require('cors');
 const { connect } = require('./utils/connect');
 const multer = require('multer');
@@ -11,6 +8,11 @@ const helmet = require('helmet');
 const { auth } = require('./middleware/authentication');
 const path = require('path');
 const app = express();
+const {createUserHandler, loginHandler, getUser, getUserFriends, addRemoveFriend} = require('./controllers/userController');
+const {createPost, getFeedPosts, getUserPosts, likePost} = require('./controllers/postController');
+const validateResource = require('./middleware/validateResource');
+const createUserSchema = require('./schemas/userSchema');
+const UserModel = require('./models/userModel');
 
 const port = config.get("port");
 
@@ -31,15 +33,41 @@ const upload = multer({
 })
 app.use('/assets', express.static('public/assets'));
 app.post('/posts', auth, upload.single("picture"));
-app.use('/', authRoutes);
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({
     policy: 'cross-origin'
 }))
 app.use(cors());
 app.use(express.json());
+
+const router = express.Router();
+
+router.get('/hello', (req, res) => {
+    res.json({
+        message: 'hello world'
+    });
+})
+router.post('/signin', validateResource(createUserSchema), createUserHandler);
+router.post('/login', validateResource(createUserSchema), loginHandler);
+router.get('/me', auth, async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ id: req.userId });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ email: user.email, id: user.id });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+router.get('/posts', auth, getFeedPosts);
+router.get('/posts/:userId/posts', auth, getUserPosts);
+router.patch('/posts/:id/like', auth, likePost);
+router.get('/users/:id', auth, getUser);
+router.get('/users/:id/friends', auth, getUserFriends);
+router.patch('/users/:id/:friendId', auth, addRemoveFriend);
+
+app.use(router)
 
 app.listen(port, async () => {
     await connect();
